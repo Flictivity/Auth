f53ba2d9e8bd510ef449bc44c0513b694b34124a
Bulat Saifullin <85502952+Flictivity@users.noreply.github.com>
2024-06-14 11:18:10 +0300

    Add files via upload

---
+++ AuthorizationPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoIlyas.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbLogin.Text))
+            {
+                MessageBox.Show("Логин не может быть пустым!", "Ошибка!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(pbPassword.Password))
+            {
+                MessageBox.Show("Пароль не может быть пустым!", "Ошибка!");
+                return;
+            }
+
+            var user = App.Connection.User.FirstOrDefault(x => x.Login == tbLogin.Text);
+
+            if (user == null)
+            {
+                MessageBox.Show("Пользователя с таким логином не существует!", "Ошибка!");
+                return;
+            }
+
+            if (user.Password != pbPassword.Password)
+            {
+                MessageBox.Show("Неверный пароль!", "Ошибка!");
+                return;
+            }
+
+            App.CurrentUser = user;
+
+            if (App.CurrentUser.RoleId == 3)
+            {
+                NavigationService.Navigate(new RequestsPage());
+            }
+        }
+    }
+}
---
+++ RequestsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DemoIlyas.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace DemoIlyas.Pages
+{
+    /// <summary>
+    /// Interaction logic for RequestsPage.xaml
+    /// </summary>
+    public partial class RequestsPage : Page
+    {
+        private List<Request> Requests = new List<Request>();
+        public RequestsPage()
+        {
+            InitializeComponent();
+            Requests = App.Connection.Request.ToList();
+            lvRequests.ItemsSource = Requests;
+            tbRequestsCount.Text = $"Количество выполненных заявок - {App.Connection.Request.Count(x => x.StatusId == 2)}";
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lvRequests.ItemsSource = Requests.Where(x => string.Join(" ", x.Description, x.Model.Name, x.User1?.FullName, x.User?.FullName, x.RequestStatus.Name).ToLower().Contains(tbSearch.Text.ToLower()));
+        }
+    }
+}
